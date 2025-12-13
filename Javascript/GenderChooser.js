f3a3555ac9d0077de7e590d34530ecdb0693a657
Call me Jerry <GarathX2@outlook.com>
2025-12-13 00:23:52 +0000

    Create GenderChooser.js

---
+++ Javascript/GenderChooser.js
@@ -0,0 +1,15 @@
+const GenderC1 = () => {
+  const Gender = 1;
+  document.getElementByClass("Intro I").style.display = "none";
+  document.getElementByClass("Intro II").style.display = "block";
+}
+const GenderC2 = () => {
+  cosnt Gender = 2;
+  document.getElementByClass("Intro I").style.display = "none";
+  document.getElementByClass("Intro II").style.display = "block";
+}
+const GenderC3 = () => {
+  const Gender = 3;
+  document.getElementByClass("Intro I").style.display = "none";
+  document.getElementByClass("Intro II").style.display = "block";
+}
