0eea4f196518a74a17cb0ac6d36821b2c2f79478
Call me Jerry <GarathX2@outlook.com>
2025-12-13 00:18:57 +0000

    Add gender selection event listeners

--- Javascript/Listeners.js
+++ Javascript/Listeners.js
@@ -1,1 +1,5 @@
 document.getElementById("Commence").addEventListener("click", Prologue);  
+
+document.getElementById("Male").addEventListener("click", GenderC1);
+document.getElementById("Female").addEventListener("click", GenderC2);
+document.getElementById("Other").addEventListener("click", GenderC3);
