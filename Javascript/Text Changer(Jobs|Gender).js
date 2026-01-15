14371753f74042c5f9d6e562f24a02eff1f4a8bf
Call me Jerry <GarathX2@outlook.com>
2026-01-15 20:20:47 +0000

    Add job and gender-based text changes

---
+++ Javascript/Text Changer(Jobs|Gender).js
@@ -0,0 +1,7 @@
+if (Job = 1) {
+  document.getElementById("Intro III").innerHTML = "You spend many years working in the forges of Heaven, under a smith named Gabrol. He's a good teacher,and you're happy to have learned from him. At the end of your tenure, you're called to the head angels office."
+} else if (Job = 2 && Gender = 1) { 
+  document.getElementById("Intro III").innerHTML = "You study for almost 40 years under the tutelage of an angel named Aravos. He teaches you how to use all manner of weapons effectively. Now that you're at the end of your tenure, you're called up to the head angel's office."
+} else {
+  document.getElementById("Intro III").innerHTML = "You spend several decades working in the Labytherian Library, the primary location for knowledge. It was a very good experience, but now your tenure has ended. You're called up to the head angels office."
+}
