fe71fd0602a319745a0ef3e9f0627685e2c6a6d3
Call me Jerry <GarathX2@outlook.com>
2026-01-15 18:31:22 +0000

    Add job chooser functions for different jobs

---
+++ Javascript/Job Chooser.js
@@ -0,0 +1,17 @@
+const JobC1 = () => {
+  const Job = 1;
+  document.getElementByClass("Intro II").style.display = "none";
+  document.getElementByClass("Intro III").style.display = "block";
+}
+
+const JobC2 = () => {
+  const Job = 2;
+  document.getElementByClass("Intro II").style.display = "none";
+  document.getElementByClass("Intro III").style.display = "block";
+}
+
+const JobC3 = () => {
+  const Job = 3;
+  document.getElementByClass("Intro II").style.display = "none";
+  document.getElementByClass("Intro III").style.display = "block";
+}
