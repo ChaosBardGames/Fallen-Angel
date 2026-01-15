2ca82552df53716c000ba112cce248d3b41a7f3e
Call me Jerry <GarathX2@outlook.com>
2026-01-15 18:33:40 +0000

    Add comments to event listener registrations

--- Javascript/Listeners.js
+++ Javascript/Listeners.js
@@ -1,5 +1,9 @@
-document.getElementById("Commence").addEventListener("click", Prologue);  
+document.getElementById("Commence").addEventListener("click", Prologue);  //Javascript that runs on launch
 
-document.getElementById("Male").addEventListener("click", GenderC1);
+document.getElementById("Male").addEventListener("click", GenderC1); //For choosing Gender
 document.getElementById("Female").addEventListener("click", GenderC2);
 document.getElementById("Other").addEventListener("click", GenderC3);
+
+document.getElementById("Hammer").addEventListener("click", JobC1); //For choosing initial job
+document.getElementById("Sword").addEventListener("click", JobC2);
+document.getElementById("Scroll").addEventListener("click", JobC3);
