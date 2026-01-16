806220ee2cf14c0aed1d4c0fe3b355ace6a2eda5
Call me Jerry <GarathX2@outlook.com>
2026-01-16 14:38:58 +0000

    Hide multiple intro screens on first load

--- Javascript/FirstLoad.js
+++ Javascript/FirstLoad.js
@@ -1,2 +1,5 @@
 document.getElementByClass("Start Screen").style.display = "block";
 document.getElementByClass("Intro I").style.display = "none";
+document.getElementByClass("Intro II").style.display = "none";
+document.getElementByClass("Intro III").style.display = "none";
+document.getElementByClass("Intro IV").style.display = "none";
